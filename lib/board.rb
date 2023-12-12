c88063ebb9f088433374dfd0704af3aa83fd7773
youyoumu <19760884+youyoumu@users.noreply.github.com>
2023-12-12 17:50:27 +0700

    Add comments

--- lib/board.rb
+++ lib/board.rb
@@ -3,19 +3,25 @@ class Board
     target_coornidate_obj = Square.new(target_coornidate)
     surrounding_coordinate = target_coornidate_obj.surrounding_coordinate
     surrounding_corner_coordinate = target_coornidate_obj.surrounding_corner_coordinate
+
     step = knight_moves_recursive(start_coordinate, target_coornidate, [], 0, surrounding_coordinate, surrounding_corner_coordinate)
+
     puts "Start: #{start_coordinate}, Target: #{target_coornidate}"
     puts "You made it in #{step.length - 1} moves! Here's your path:"
     p step
   end
 
   def knight_moves_recursive(start_coordinate, target_coornidate, step, counter, surrounding_coordinate, surrounding_corner_coordinate)
+    #return nil if the step is surpassing 10
     return if counter == 10
 
     step.push(start_coordinate)
     start_coordinate = Square.new(start_coordinate)
+
+    #get the final result
     return step.push(target_coornidate) if start_coordinate.knight_possible_move.include?(target_coornidate)
 
+    #get the nearest step to the target while avoiding some coordinate
     next_step = start_coordinate.knight_possible_move.reduce(start_coordinate.coordinate) do |best_coordinate, coordinate|
       best_coordinate_distance = get_coordinate_distance(best_coordinate, target_coornidate)
       current_coordinate_distance = get_coordinate_distance(coordinate, target_coornidate)
@@ -35,6 +41,7 @@ class Board
     knight_moves_recursive(next_step, target_coornidate, step, counter + 1, surrounding_coordinate, surrounding_corner_coordinate)
   end
 
+  #calculate the distance between two coordinates
   def get_coordinate_distance(start_coordinate, target_coornidate)
     a = target_coornidate[0] - start_coordinate[0]
     b = target_coornidate[1] - start_coordinate[1]
